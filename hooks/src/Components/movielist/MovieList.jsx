f40df38706cc0b852a19a7bb9eafa94042ef4c49
adeleye20 <adeleyedaniel655@gmail.com>
2023-03-15 11:55:55 +0100

    first commit

---
+++ hooks/src/Components/movielist/MovieList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import MovieCard from '../moviecard/MovieCard';
+
+const MovieList = ({ movies }) => {
+  return (
+    <div className="movie-list">
+      {movies.map((movie) => (
+        <MovieCard key={movie.id} {...movie} />
+      ))}
+    </div>
+  );
+};
+
+export default MovieList;
